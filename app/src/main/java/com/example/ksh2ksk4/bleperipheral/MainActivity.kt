5b6444ef840bd6d0f4594473e3ca3ddb5b30ce3d
KISHI Kousuke <ksh2ksk4@gmail.com>
2017-07-23 06:50:31 +0900

    Check whether Bluetooth is enabled

--- app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
+++ app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.ksh2ksk4.bleperipheral
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +13,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.java.name
+        private val REQUEST_ENABLE_BLUETOOTH = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,15 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "This device does not support BLE.", Toast.LENGTH_SHORT).show()
 
             finishAndRemoveTask()
+        }
+
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+
+        // Bluetoothが有効になっているかどうかチェック
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
     }
 }
