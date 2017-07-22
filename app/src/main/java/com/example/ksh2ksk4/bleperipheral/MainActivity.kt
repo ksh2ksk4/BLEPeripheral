bd42e0f9b6c1feb9dc325bed00a6c68b047afc28
KISHI Kousuke <ksh2ksk4@gmail.com>
2017-07-23 05:39:32 +0900

    Starting off BLEPeripheral

---
+++ app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ksh2ksk4.bleperipheral
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
