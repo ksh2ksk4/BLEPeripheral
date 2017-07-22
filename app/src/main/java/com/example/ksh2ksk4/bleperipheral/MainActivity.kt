f73a81d88b4c849fde3c6ca217702dc05cd01179
KISHI Kousuke <ksh2ksk4@gmail.com>
2017-07-23 06:28:37 +0900

    Check whether devices support BLE

--- app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
+++ app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.ksh2ksk4.bleperipheral
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.name
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // デバイスがBLEをサポートしているかチェック
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.d(TAG, "This device does not support BLE.")
+            Toast.makeText(this, "This device does not support BLE.", Toast.LENGTH_SHORT).show()
+
+            finishAndRemoveTask()
+        }
     }
 }
