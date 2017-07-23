aa2d27c616d14c4fd44ec40058dcb80dc240e982
KISHI Kousuke <ksh2ksk4@gmail.com>
2017-07-23 17:44:56 +0900

    Send iBeacon advertising packets

--- app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
+++ app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ksh2ksk4.bleperipheral
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
 import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.content.Intent
@@ -13,11 +16,17 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import java.nio.ByteBuffer
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.java.name
         private val REQUEST_ENABLE_BLUETOOTH = 1
+        private val ADVERTISING_TIMEOUT = 4000
+        private val IBEACON_UUID = "7694505b-707b-484c-82ce-6917eac8191e"
+        private val IBEACON_MAJOR = 831
+        private val IBEACON_MINOR = 5555
     }
 
     lateinit private var textViewMessage: TextView
@@ -79,13 +88,107 @@ class MainActivity : AppCompatActivity() {
     /**
      * Advertising
      */
+    private val advertiseSettings = buildAdvertiseSettings()
+    private val advertiseData = buildAdvertiseData()
+    private val advertiseCallbackIBeacon = AdvertiseCallbackIBeacon()
+
+    private val uuid: ByteArray
+        get() {
+            val uuid = UUID.fromString(IBEACON_UUID)
+            val byteBuffer = ByteBuffer.wrap(ByteArray(16))
+            byteBuffer.putLong(uuid.mostSignificantBits)
+            byteBuffer.putLong(uuid.leastSignificantBits)
+
+            return byteBuffer.array()
+        }
+
+    private var isAdvertising = false
+
+    private fun buildAdvertiseSettings(): AdvertiseSettings {
+        return AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(false)
+                .setTimeout(ADVERTISING_TIMEOUT)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_LOW)
+                .build()
+    }
+
+    private fun buildAdvertiseData(): AdvertiseData {
+        val manufacturerData = byteArrayOf(
+                // 0x02: iBeacon
+                0x02,
+                // 0x15: 21Bytes
+                0x15,
+                // UUID(16Bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                // major
+                IBEACON_MAJOR.ushr(8).toByte(),
+                IBEACON_MAJOR.toByte(),
+                // minor
+                IBEACON_MINOR.ushr(8).toByte(),
+                IBEACON_MINOR.toByte(),
+                // measured power
+                (AdvertiseSettings.ADVERTISE_TX_POWER_HIGH - 41).toByte())
+        System.arraycopy(uuid, 0, manufacturerData, 2, 16)
+
+        Log.d(TAG, "manufacturerData: " + manufacturerData.toString())
+
+        return AdvertiseData.Builder()
+                // 0x004c: Apple
+                .addManufacturerData(0x004c, manufacturerData)
+                .setIncludeDeviceName(false)
+                .setIncludeTxPowerLevel(false)
+                .build()
+    }
+
+    private inner class AdvertiseCallbackIBeacon: AdvertiseCallback() {
+        override fun onStartSuccess(advertiseSettings: AdvertiseSettings) {
+            super.onStartSuccess(advertiseSettings)
+
+            Log.d(TAG, "Advertising succeeded.")
+            textViewMessage.append("Advertising succeeded.\n")
+        }
+
+        override fun onStartFailure(errorCode: Int) {
+            super.onStartFailure(errorCode)
+
+            Log.d(TAG, "Advertising failed.")
+            textViewMessage.append("Advertising failed.\n")
+        }
+    }
+
     private fun startAdvertising() {
         Log.d(TAG, "Advertising started.")
         textViewMessage.append("Advertising started.\n")
+
+        isAdvertising = true
+
+        val thread = Thread(Runnable {
+            while (isAdvertising) {
+                Log.d(TAG, "waking")
+
+                bluetoothLeAdvertiser.startAdvertising(advertiseSettings, advertiseData, advertiseCallbackIBeacon)
+
+                try {
+                    Log.d(TAG, "sleeping")
+
+                    Thread.sleep((ADVERTISING_TIMEOUT + 2000).toLong())
+                } catch (e: InterruptedException) {
+                    Log.d(TAG, e.toString())
+                }
+            }
+        })
+
+        thread.start()
     }
 
     private fun stopAdvertising() {
         Log.d(TAG, "Advertising stopped.")
         textViewMessage.append("Advertising stopped.\n")
+
+        bluetoothLeAdvertiser.stopAdvertising(advertiseCallbackIBeacon)
+
+        isAdvertising = false
     }
 }
