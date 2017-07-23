6a134d18ddf3e70993096063848674ac827729b8
KISHI Kousuke <ksh2ksk4@gmail.com>
2017-07-23 15:22:41 +0900

    Check whether devices support facility of peripheral

--- app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
+++ app/src/main/java/com/example/ksh2ksk4/bleperipheral/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.ksh2ksk4.bleperipheral
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
 
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.name
         private val REQUEST_ENABLE_BLUETOOTH = 1
     }
+
+    lateinit private var bluetoothLeAdvertiser: BluetoothLeAdvertiser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +39,21 @@ class MainActivity : AppCompatActivity() {
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+
+        //fixme アプリ開始前にBluetoothが有効になっていないと、ここでNPEが発生
+        bluetoothLeAdvertiser = bluetoothAdapter!!.bluetoothLeAdvertiser
+
+        // peripheralの機能をサポートしているかチェック
+        if (Build.VERSION.SDK_INT <= 20 ||
+                bluetoothLeAdvertiser == null ||
+                !bluetoothAdapter.isMultipleAdvertisementSupported ||
+                !bluetoothAdapter.isOffloadedFilteringSupported ||
+                !bluetoothAdapter.isOffloadedScanBatchingSupported) {
+            Log.d(TAG, "This device does not support facility of peripheral")
+            Toast.makeText(this, "This device does not support facility of peripheral", Toast.LENGTH_SHORT).show()
+
+            finishAndRemoveTask()
         }
     }
 }
